0bb52956866cc0ec573b5bdde0d50263bd3f7e79
Dana <danaciocoiu78@gmail.com>
2018-04-20 17:38:52 +0300

    crud in progress

---
+++ WebApplicationCRUD/WebApplicationCRUD/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WebApplicationCRUD.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ WebApplicationCRUD/WebApplicationCRUD/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApplicationCRUD.WebApiApplication" Language="C#" %>
